7f5b425a627ee08f3060d237b7efb80adb6c857e
dev <devjeetacharya003@gmail.com>
2025-11-10 22:14:22 +0530

    Updated code with new changes

--- assets/js/services.js
+++ assets/js/services.js
@@ -239,7 +239,7 @@ function loadAllSheetIDsIntoAccordion(workbook) {
     if (sheetRow.Image1) { // or Image2 depending on your sheet
         imageHTML = `
             <div class="breadcrumb-sub-title">
-                <img src="${sheetRow.Image1}" alt="${sheetRow.ID}" style="max-width: 100%; height:200px;">
+                <img src="${sheetRow.Image1}" alt="${sheetRow.ID}" style="max-width: 100%;">
             </div>
         `;
     }
@@ -253,7 +253,7 @@ function loadAllSheetIDsIntoAccordion(workbook) {
                 </h4>
             </div>
             <div class="offcanvas__close">
-                <button onclick="toggleOffcanvas('offcanvas1')">
+                <button onclick="toggleOffcanvas('offcanvas2')">
                     <i class="fas fa-times"></i>
                 </button>
             </div>
